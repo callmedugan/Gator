5a844e49b2617337be7dfabfbbde77731e646ad4
callmedugan <callmedugan@gmail.com>
2026-07-17 01:02:25 -0700

    set up postgres and drizzle

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import { CommandsRegistry, handlerLogin, registerCommand, runCommand } from "./c
 import process, { exit } from "node:process";
 
 function main() {
+  // create registry and add login command
   const registry:CommandsRegistry = {};
   registerCommand(registry, "login", handlerLogin);
+  //get the args that were supplied with the start command
   const args = process.argv.slice(2);
   if(args.length === 0){
     console.log("missing arguments");
     exit(1);
   }
+  //pass args to the run command which checks to see if a valid command was supplied and execute
   runCommand(registry, args[0], ...args.slice(1));
 }
 
